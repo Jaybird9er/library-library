de69bcc7dbadaeb590ee120e3ec05b6027c2a2ff
Jamey <JameyKirk@gmail.com>
2025-02-14 13:40:50 +0900

    ready for backend development

---
+++ src/app/database/connection_provider.py
@@ -0,0 +1,23 @@
+from sqlalchemy.ext.asyncio import create_async_engine
+
+from app.settings import Settings
+
+# We use this as a hidden, module-level object to ensure we re-use it.
+__engine = None
+
+
+async def database_connection():
+    # Make sure we use the module-level object.
+    global __engine
+    if __engine is None:
+        settings = Settings()
+        connection_string = settings.get_db_url()
+        # This stores the SQLAlchemy engine back in the module-level object.
+        # This ensures we don't accidentally create multiple connection pools.
+        __engine = create_async_engine(connection_string)
+
+    # https://docs.sqlalchemy.org/en/20/tutorial/dbapi_transactions.html#committing-changes
+    # Automatically create a transaction. Rollback on error. Commit on completion of the context.
+    async with __engine.begin() as connection:
+        # Yield to ensure we return back to this context in order to commit properly.
+        yield connection
