a01fe449a17cf21eacdf75a39f3e600674325c7f
Jamey <JameyKirk@gmail.com>
2025-02-14 13:16:38 +0900

    readying for development

--- src/app/main.py
+++ src/app/main.py
@@ -1,13 +1,29 @@
 import uvicorn
 from fastapi import FastAPI
 
+# Block comments are route examples
+""" 
+from app.routes.product import router as product_router
+"""
 from app.settings import Settings
 
-app = FastAPI()
+# Don't forget to create your Settings object to use it!
 settings = Settings()
+# We'll store our main FastAPI application in this app variable
+app = FastAPI()
+# We can define routes on the FastAPI application directly, or
+# we can create a separate router that can be included in the app
+# logic. Note that we can define a prefix and organize routers by
+# prefix. This allows you to logically split your application logic.
+"""
+app.include_router(product_router, prefix="/products")
+"""
 
-
+# From our pyproject.toml, we define this main function as our entrypoint.
 def main():
+    # Here, we run the FastAPI application under the Uvicorn ASGI server.
+    # Note that we could also run uvicorn via the CLI directly:
+    #   uvicorn --host 0.0.0.0 --port 5000 "app.main:app"
     uvicorn.run(
         "app.main:app",
         host=settings.host_address,
--- src/app/settings.py
+++ src/app/settings.py
@@ -1,6 +1,24 @@
 from pydantic_settings import BaseSettings
 
 
+# This Settings class automatically retrieves environment variables and stores them
+# in these class variables. Environment variable names are expected to be in all caps.
+# Type validation is done automatically.
 class Settings(BaseSettings):
+    public_base_url: str = "http://localhost:8000"
     host_address: str = "0.0.0.0"
     port: int = 8000
+    db_host: str = "localhost"
+    db_port: int = 5432
+    db_username: str = "root"
+    db_password: str = "root"
+    db_database: str = "ecommerce"
+
+    def get_db_url(self):
+        return "postgresql+asyncpg://{username}:{password}@{host}:{port}/{database}".format(
+            username=self.db_username,
+            password=self.db_password,
+            host=self.db_host,
+            port=self.db_port,
+            database=self.db_database,
+        )
